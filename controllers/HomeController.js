b69b38ae9aa447d5f6b1835f309222e39898ccd1
Ryan <ryannarelli12@gmail.com>
2024-11-24 02:19:48 -0300

    enviando ajuste

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -2,7 +2,8 @@ module.exports = class HomeController {
     static  showHome(req, res) {
         try {
             const successRegister = req.query.successRegister === 'true';
-            res.render('home/home', { showHeaderFooter: true, successRegister});
+            const isAuthenticated = req.session.login; 
+            res.render('home/home', { showHeaderFooter: true, successRegister, isAuthenticated });
         } catch(err) {
             console.log('Erro ao renderizar a home');
         }
